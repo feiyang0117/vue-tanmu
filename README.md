5b0ee0bc00efd11ae29c08d369219c06da73c97b
gaomiaofei <gaomiaofei@gaomiaofeis-MacBook-Pro.local>
2018-12-03 00:04:33 +0800

    add index.js

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # vue-tanmu
 基于vue弹幕
+
+# 使用方式
+npm i vue-tanmu
---
+++ index.js
@@ -0,0 +1,6 @@
+import VueTanmu from './vue-tanmu.vue'
+VueTanmu.install = function (Vue) {
+  Vue.component('VueTanmu', VueTanmu)
+}
+
+export default VueTanmu
